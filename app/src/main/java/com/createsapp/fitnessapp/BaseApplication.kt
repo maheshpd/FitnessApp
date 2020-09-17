17c76cd16fb3e85a24b3f59191da89ab2bd8b70b
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-09-17 13:19:52 +0530

    Dagger-Hilt Setup - MVVM Running Tracker App - Part 6

---
+++ app/src/main/java/com/createsapp/fitnessapp/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.createsapp.fitnessapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
--- app/src/main/java/com/createsapp/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/createsapp/fitnessapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.createsapp.fitnessapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.createsapp.fitnessapp.db.RunDAO
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
